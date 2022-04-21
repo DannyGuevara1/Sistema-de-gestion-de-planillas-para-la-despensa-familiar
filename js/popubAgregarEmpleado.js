cd43d856111b9573175fe4f63a2aa323708647b4
DannyGuevara1 <69401899+DannyGuevara1@users.noreply.github.com>
2022-04-21 16:52:06 -0600

    Add files via upload

---
+++ js/popubAgregarEmpleado.js
@@ -0,0 +1,15 @@
+$(function(){
+    $("#open").on("click" , function(){
+    $("#popup").show();
+    });
+
+    $("#close").on("click" , function(){
+        $("#popup").hide();
+        });
+    
+});
+$(function(){
+    $("#close_ac").on("click" , function(){
+        $("#popup_ac").hide();
+    });
+})
